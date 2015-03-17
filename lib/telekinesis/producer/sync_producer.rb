e21e7fac01c209dd8b2bb60d6421cd9dc7093386
Ben Linsay <ben@kickstarter.com>
2015-03-17 14:35:21 -0400

    clean up sync producer

--- lib/telekinesis/producer/sync_producer.rb
+++ lib/telekinesis/producer/sync_producer.rb
@@ -11,13 +11,12 @@ module Telekinesis
     end
 
     def put(key, data)
-      client.put_record(build_request(key, data))
+      client.put_record(stream: stream, data: data, partition_key: key)
     end
 
     def put_all(items)
       items.each_slice(MAX_BUFFER_SIZE).each do |batch|
-        data = batch.map{|k, v| {partition_key: k, data: v}}
-        failures = client.put_records(stream, data).flat_map do |page|
+        failures = put_records(batch).flat_map do |page|
           page.records.reject{|r| r.error_code.nil?}
         end
         on_failure(failures.size, failures) if (failures.size > 0)
@@ -26,6 +25,15 @@ module Telekinesis
 
     def on_failure(failures)
       Telekinesis.logger.error("put_records returned #{failures.size} failures")
+    end
+
+    protected
+
+    def put_records(items)
+      client.put_records(
+        stream: stream,
+        records: items.map{|k, v| {partition_key: k, data: v}}
+      )
     end
   end
 end
