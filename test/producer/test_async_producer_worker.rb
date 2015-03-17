02af33080f61932e8b87e65f7298a2d4e9a5909d
Ben Linsay <ben@kickstarter.com>
2015-03-17 18:41:07 -0400

    add some more tests

--- test/producer/test_async_producer_worker.rb
+++ test/producer/test_async_producer_worker.rb
@@ -8,8 +8,14 @@ java_import java.util.concurrent.ArrayBlockingQueue
 
 
 class AsyncProducerWorkerTest < Minitest::Test
-  StubProducer = Struct.new(:stream, :client)
+  StubProducer = Struct.new(:stream, :client) do
+    def on_failure(failures)
+      @failures = failures
+    end
+  end
+
   StubResponse = Struct.new(:failed_record_count, :get_records)
+  StubResponseEntry = Struct.new(:error_code)
 
   class CapturingClient
     attr_reader :results
@@ -20,8 +26,8 @@ class AsyncProducerWorkerTest < Minitest::Test
     end
 
     def put_records(request)
-      @results.puts(request)
-      @responses.shift
+      @results.put(request)
+      @responses.shift || StubResponse.new(0, [])
     end
   end
 
@@ -35,7 +41,7 @@ class AsyncProducerWorkerTest < Minitest::Test
       @send_every = 25
     end
 
-    context "with no items in a queue" do
+    context "with no items in the queue" do
       setup do
         @producer = stub_producer('test')
         @queue = ArrayBlockingQueue.new(10)
@@ -52,7 +58,7 @@ class AsyncProducerWorkerTest < Minitest::Test
       end
     end
 
-    context "with a shutdown item in the queue" do
+    context "with only SHUTDOWN in the queue" do
       setup do
         @producer = stub_producer('test')
         @queue = ArrayBlockingQueue.new(10).tap do |q|
@@ -64,6 +70,29 @@ class AsyncProducerWorkerTest < Minitest::Test
 
       should "shut down the worker" do
         assert(@worker_thread.join((@send_every * 1000).to_f))
+      end
+    end
+
+    context "with [item, SHUTDOWN] in the queue" do
+      setup do
+        @producer = stub_producer('test')
+        @queue = ArrayBlockingQueue.new(10).tap do |q|
+          q.put(["key", "value"])
+          q.put(Telekinesis::AsyncProducerWorker::SHUTDOWN)
+        end
+        @worker = Telekinesis::AsyncProducerWorker.new(@producer, @queue, @send_size, @send_every)
+        @worker_thread = Thread.new{ @worker.run }
+      end
+
+      should "put data before shutting down the worker" do
+        assert(@worker_thread.join((@send_every * 1000).to_f), "worker should shut down")
+
+        request, = @producer.client.results.to_a
+        assert_equal(request.stream_name, 'test',
+                     "request should have the correct stream name")
+        assert_equal([["key", "value"]],
+                     request.records.map{|r| [r.partition_key, string_from_bytebuffer(r.data)]},
+                     "Payload should be a nested array")
       end
     end
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,3 +7,6 @@ require "telekinesis/stats"
 require "telekinesis/logging"
 require "telekinesis/telekinesis-#{Telekinesis::VERSION}.jar"
 
+def string_from_bytebuffer(bb)
+  String.from_java_bytes bb.array
+end
