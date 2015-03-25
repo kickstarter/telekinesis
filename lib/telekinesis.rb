e2ad4a067e69bf3437142f5b4a9f59f77fa4f3c7
Ben Linsay <ben@kickstarter.com>
2015-03-25 14:25:40 -0400

    clear out top-level cruft

--- lib/telekinesis.rb
+++ lib/telekinesis.rb
@@ -1,47 +1,4 @@
-require "java"
-# Make sure that commons logging uses JUL. Set the system property before
-# the JAR is loaded to make sure that it's picked up as soon as possible.
-java.lang.System.set_property("org.apache.commons.logging.Log",
-                              "org.apache.commons.logging.impl.Jdk14Logger")
+module Telekinesis; end
 
-require "telekinesis/version"
-require "telekinesis/telekinesis-#{Telekinesis::VERSION}.jar"
+require "telekines/producer"
 
-require "telekinesis/logging"
-require "telekinesis/stats"
-require "telekinesis/config"
-require "telekinesis/producer"
-require "telekinesis/consumer/block_consumer"
-
-java_import com.amazonaws.services.kinesis.AmazonKinesisClient
-
-Telekinesis::Logging.capture_java_logging
-
-module Telekinesis
-  FIFTY_KB = 50 * 1024
-  DEFAULT_DELIMITER = "\n"
-
-  class << self
-    def producer(config_hash = {}, &block)
-      stream, creds_provider = Config.producer_config(config_hash)
-      client = AmazonKinesisClient.new(creds_provider)
-      serializer = block_given? ? block : default_serializer
-      Producer.new(stream, client, config_hash, &serializer)
-    end
-
-    def process_records(config_hash = {}, &block)
-      consumer(config_hash) { BlockRecordProcessor.new(&block) }
-    end
-
-    def consumer(config_hash = {}, &block)
-      # Closure conversion generates an IRecordProcessorFactory from &block
-      com.kickstarter.jruby.Telekinesis::new_worker(Config.consumer_config(config_hash), &block)
-    end
-
-    protected
-
-    def default_serializer
-      Proc.new { GZIPDelimitedSerializer.new(FIFTY_KB, DEFAULT_DELIMITER) }
-    end
-  end
-end
