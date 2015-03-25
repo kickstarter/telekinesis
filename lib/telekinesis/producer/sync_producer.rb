52209b1ddbfa6f18124943bc26a4288552987457
Ben Linsay <ben@kickstarter.com>
2015-03-25 14:57:09 -0400

    minor SyncProducer tweaks
    - create method
    - @send_size instead of @max_batch_size for consistency

--- lib/telekinesis/producer/sync_producer.rb
+++ lib/telekinesis/producer/sync_producer.rb
@@ -3,10 +3,16 @@ module Telekinesis
     class SyncProducer
       attr_reader :stream, :client
 
+      def self.create(options = {})
+        stream = options[:stream]
+        client = Telekinesis::Aws::Client.build(options.fetch(:credentials, {}))
+        new(stream, client, options)
+      end
+
       def initialize(stream, client, opts = {})
-        @stream = stream
-        @client = client
-        @max_batch_size = opts.fetch(:max_batch_size, Telekinesis::Aws::KINESIS_MAX_PUT_RECORDS_SIZE)
+        @stream = stream or raise ArgumentError, "stream may not be nil"
+        @client = client or raise ArgumentError, "client may not be nil"
+        @send_size = opts.fetch(:max_batch_size, Telekinesis::Aws::KINESIS_MAX_PUT_RECORDS_SIZE)
       end
 
       def put(key, data)
@@ -14,7 +20,7 @@ module Telekinesis
       end
 
       def put_all(items)
-        items.each_slice(@max_batch_size).each do |batch|
+        items.each_slice(@send_size).each do |batch|
           failures = put_records(batch).flat_map do |page|
             page.records.reject{|r| r.error_code.nil?}
           end
