97daec02c2667f2c060e219042a90dafc26eb516
Ben Linsay <ben@kickstarter.com>
2015-03-26 11:14:43 -0400

    actually require version

--- lib/telekinesis.rb
+++ lib/telekinesis.rb
@@ -1,5 +1,6 @@
 module Telekinesis; end
 
+require "telekinesis/version"
 require "telekinesis/telekinesis-#{Telekinesis::VERSION}.jar" if RUBY_PLATFORM.match(/java/)
 require "telekinesis/aws"
 require "telekinesis/producer"
